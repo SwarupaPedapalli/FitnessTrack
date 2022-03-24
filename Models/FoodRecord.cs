2256507742318ce527d3a04ad6c7ceb5c8cf9024
Satyachowdary97 <77590874+Satyachowdary97@users.noreply.github.com>
2022-03-23 21:18:45 -0500

    edited code changes

---
+++ Models/FoodRecord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessTracker.Models
+{
+    public class FoodRecord
+    {
+        public long ID { get; set; }
+
+        [Required]
+        public FitnessUser User { get; set; }
+        [Required]
+        [ForeignKey("Food")]
+        public long FoodID { get; set; }
+        public Food Food { get; set; }
+        [Required]
+        public DateTime ConsumptionDate { get; set; }
+        [Required]
+        public float Quantity { get; set; }
+    }
+}
